b88ef1e44b21a3118c586afbe839b96cbd04f53e
Manmohan Krishna <krishna.m.zyw@gmail.com>
2023-06-26 01:37:39 +0530

    Functor | more examples

--- src/main/scala/abstractmath/Functor.scala
+++ src/main/scala/abstractmath/Functor.scala
@@ -1,11 +1,60 @@
 package abstractmath
 
+import scala.util.Try
+import scala.concurrent.ExecutionContext.Implicits.global
+
 trait Functor[F[_]]:
-  def map[A, B](a: F[A])(f: A => B): F[B]
+  def map[A, B](fa: F[A])(f: A => B): F[B]
 
 object Functor:
   def apply[F[_]: Functor] = summon[Functor[F]]
 
 extension [F[_]: Functor, A](fa: F[A])
-  def map[B](f: A => B): F[B] = Functor[F].map(fa)(f)
+  def mapE[B](f: A => B): F[B] = Functor[F].map(fa)(f)
 
+object FunctorInstances:
+  given Functor[List] with
+    def map[A, B](fa: List[A])(f: A => B): List[B] =
+      fa.map(f)
+  given Functor[Option] with
+    def map[A, B](fa: Option[A])(f: A => B): Option[B] =
+      fa.map(f)
+  given Functor[Try] with
+    def map[A, B](fa: Try[A])(f: A => B): Try[B] =
+      fa.map(f)
+
+sealed trait Tree[+T]
+case class Leaf[+T](value: T) extends Tree[T]
+case class Branch[+T](left: Tree[T], value: T, right: Tree[T]) extends Tree[T]
+
+object Tree:
+  def leaf[T](value: T): Tree[T] = Leaf(value)
+  def branch[T](leftT: Tree[T], value: T, rightT: Tree[T]): Tree[T] = 
+    Branch(leftT, value, rightT)
+  given Functor[Tree] with
+    def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match
+      case Leaf(value) => Leaf(f(value))
+      case Branch(leftT, value, rightT) => 
+        Branch(map(leftT)(f), f(value), map(rightT)(f))
+
+
+object FunctorPlayground extends App:
+  import FunctorInstances.given
+  val numbers = List(1, 2, 3)
+  println(Functor[List].map(numbers)(_ + 1))
+  println(numbers.mapE(_ + 1))
+
+  val intOption = Option(1)
+  println(Functor[Option].map(intOption)(_ + 1))
+  println(intOption.mapE(_ + 1))
+
+  val aTry = Try(1)
+  println(Functor[Try].map(aTry)(_ + 1))
+  println(aTry.mapE(_ + 1))
+
+  val myTree = Tree.branch(Tree.branch(Tree.leaf(4), 2, Tree.leaf(5)), 1, Tree.branch(Tree.leaf(6), 3, Tree.leaf(7)))
+  val doubledTree = myTree.mapE(_ * 2)
+  println(doubledTree)
+
+
+  
